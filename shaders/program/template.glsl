c074b0d46007ddbf4c88898a3c608feacf3394b1
Gabpro <gablimagamer@gmail.com>
2026-04-24 19:41:21 -0300

    first upload

---
+++ shaders/program/template.glsl
@@ -0,0 +1,44 @@
+///////////////////////////////////////////////////////////////
+/// VoidBlue Shaders
+/// by Gab_pro
+/// Based on Complementary Shaders by EminGT
+///////////////////////////////////////////////////////////////
+
+//Common//
+#include "/lib/common.glsl"
+
+//////////Fragment Shader//////////Fragment Shader//////////Fragment Shader//////////
+#ifdef FRAGMENT_SHADER
+
+//Pipeline Constants//
+
+//Common Variables//
+
+//Common Functions//
+
+//Includes//
+
+//Program//
+void main() {
+
+}
+
+#endif
+
+//////////Vertex Shader//////////Vertex Shader//////////Vertex Shader//////////
+#ifdef VERTEX_SHADER
+
+//Attributes//
+
+//Common Variables//
+
+//Common Functions//
+
+//Includes//
+
+//Program//
+void main() {
+
+}
+
+#endif
